d71dca59c883af1ffc6d32ff8f1da5481af82fed
Pushpajekumar <mk6358522@gmail.com>
2024-09-02 12:59:30 +0530

    setup theme

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,14 @@
 import * as React from "react";
 import { View } from "react-native";
-
 import { Button } from "~/components/ui/button";
-
 import { Text } from "~/components/ui/text";
 
 export default function Screen() {
   return (
     <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
-      <Text className="native:text-lg">Freelance</Text>
-      <Button variant="outline" className="shadow shadow-foreground/5">
-        <Text>Update</Text>
+      <Text className="text-2xl font-bold text-primary">Hello, World!</Text>
+      <Button>
+        <Text className="text-lg text-primary">Press me</Text>
       </Button>
     </View>
   );
